2f3fe1f739f6d32239faf62d4180d5a814d8250e
Christopher Larson <kergoth@gmail.com>
2018-08-30 09:37:06 -0700

    Swap alt & gui in my tokyo60 keymap
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- keyboards/tokyo60/keymaps/kergoth/keymap.c
+++ keyboards/tokyo60/keymaps/kergoth/keymap.c
@@ -3,7 +3,10 @@
 #define BL 0
 #define FN 1
 /*
- * Default HHKB Layout
+ * HHKB Layout, esc remapped to `, ` remapped to hyper, control as ctrl+esc,
+ * fn+z/x/c as media keys,x,c, fn+backspace as reset. alt and gui are swapped
+ * from default and swapped back on my macs in software, as it's easier to
+ * remap mods on the mac than in windows.
  */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -25,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,           KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSPC,      \
         CTL_T(KC_ESC),    KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,       KC_ENT,      \
         KC_LSFT,                   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M, KC_COMM,  KC_DOT, KC_SLSH,      KC_RSFT, MO(FN), \
-             KC_LALT, KC_LGUI,              KC_SPC,                         KC_RGUI, KC_RALT ),
+             KC_LGUI, KC_LALT,              KC_SPC,                         KC_RALT, KC_RGUI),
     /* 1: HHKB Fn layer
 ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
 │ Pwr │ F1  │ F2  │ F3  │ F4  │ F5  │ F6  │ F7  │ F8  │ F9  │ F10 │ F11 │ F12 │ Ins │ Del │
